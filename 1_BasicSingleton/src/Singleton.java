7acc669f2b81f3798eb7b077e4e6e353465ddef2
PrabhGhai <ghaiprabhghai@gmail.com>
2026-03-09 21:38:27 +0530

    Basic Singleton

---
+++ 1_BasicSingleton/src/Singleton.java
@@ -0,0 +1,15 @@
+public class Singleton {
+
+    private static Singleton INSTANCE = null;
+    private Singleton() {}
+
+    public static Singleton getInstance()
+    {
+        if(INSTANCE == null)
+        {
+            INSTANCE = new Singleton();
+        }
+
+        return INSTANCE;
+    }
+}
