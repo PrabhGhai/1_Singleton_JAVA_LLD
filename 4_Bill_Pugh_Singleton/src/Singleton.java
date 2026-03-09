4dbaa47fdf37209d330b7ed58d014f68cf626551
PrabhGhai <ghaiprabhghai@gmail.com>
2026-03-09 22:38:50 +0530

    Bill Pugh Singleton

---
+++ 4_Bill_Pugh_Singleton/src/Singleton.java
@@ -0,0 +1,14 @@
+public class Singleton {
+
+    private Singleton() {}
+
+    private static class SingletonHolder{
+        private static Singleton INSTANCE  = new Singleton();
+    }
+
+    public static Singleton getInstance()
+    {
+        return SingletonHolder.INSTANCE;
+
+    }
+}
